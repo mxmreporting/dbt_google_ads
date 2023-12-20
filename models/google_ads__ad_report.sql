ace9832cf35e8745dffe26e4385997c70e3c2665
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-20 20:29:47 +0530

    Update google_ads__ad_report.sql

--- models/google_ads__ad_report.sql
+++ models/google_ads__ad_report.sql
@@ -90,6 +90,6 @@ where DATE(date_day) >= DATE_ADD(CURRENT_DATE(), INTERVAL -2 YEAR)
 {% if is_incremental() %}
 
 
-  AND date_day >= (select max(date_day) from {{ this }})
+  AND date_day >= (select DATE_ADD(max(date_day), INTERVAL -3 Day) from {{ this }})
 
 {% endif %}
